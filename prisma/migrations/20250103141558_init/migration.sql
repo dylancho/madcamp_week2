9ad138e531b07abe98da81910a76fce2a1fcacf8
Doun Lee <jusmint3@gmail.com>
2025-01-03 23:16:49 +0900

    add : user & map DB

---
+++ prisma/migrations/20250103141558_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'Nupzuk',
+    "passward" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Map" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'new experiment',
+    "creatorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "config" JSONB NOT NULL,
+
+    CONSTRAINT "Map_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Map" ADD CONSTRAINT "Map_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
